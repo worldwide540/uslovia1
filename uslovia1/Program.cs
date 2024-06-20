9669f3527f4bf2279407648232797be8de264d10
Нарек Антонян <narekantonyan2033@gmail.com>
2024-06-20 18:26:48 +0300

    Добавьте файлы проекта.

---
+++ uslovia1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        if (num1 == num2)
+        {
+            Console.WriteLine("Числа равны");
+        }
+        else if (num1 > num2)
+        {
+            Console.WriteLine("Первое число больше второго");
+        }
+        else
+        {
+            Console.WriteLine("Первое число меньше второго");
+        }
+    }
+}
